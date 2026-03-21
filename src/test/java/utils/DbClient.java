8c7d1c863625d2065d7c55f930197295da6b7d88
nikita.elakov <nelakov24@gmail.com>
2026-03-21 13:05:03 +0400

    Rename DbUtils to DbClient, eliminate duplication and dead code
    - Rename DbUtils → DbClient (reflects role)
    - Extract getConnection() to eliminate 3x connection creation
    - Replace countRecords() body with getData() call (DRY)
    - Remove dead COUNT query after DELETE in clearTables()
    - Remove all System.out.println side effects
    - Replace lombok.val with Java var
    - Make fields private static final

---
+++ src/test/java/utils/DbClient.java
@@ -0,0 +1,48 @@
+package utils;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbClient {
+    private static final String url = System.getProperty("db.url");
+    private static final String user = System.getProperty("db.user");
+    private static final String password = System.getProperty("db.password");
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void clearTables() throws SQLException {
+        var runner = new QueryRunner();
+
+        try (var conn = getConnection()) {
+            runner.update(conn, "DELETE FROM order_entity;");
+            runner.update(conn, "DELETE FROM payment_entity;");
+            runner.update(conn, "DELETE FROM credit_request_entity;");
+        }
+    }
+
+    public static String findPaymentStatus() throws SQLException {
+        return getData("SELECT status FROM payment_entity;");
+    }
+
+    public static String findCreditStatus() throws SQLException {
+        return getData("SELECT status FROM credit_request_entity;");
+    }
+
+    public static String countRecords() throws SQLException {
+        return getData("SELECT COUNT(*) FROM order_entity;");
+    }
+
+    private static String getData(String query) throws SQLException {
+        var runner = new QueryRunner();
+        try (var conn = getConnection()) {
+            return String.valueOf(runner.query(conn, query, new ScalarHandler<>()));
+        }
+    }
+
+}
